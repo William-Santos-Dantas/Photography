5ff7d0c581c16630648be46589969fe25f55e37b
unknown <NidrahDev@protonmail.com>
2021-02-16 18:33:16 -0300

    carousel + images

--- src/pages/index/components/AppBar.vue
+++ src/pages/index/components/AppBar.vue
@@ -1,22 +1,46 @@
 <template>
-  <div>
-    <v-app-bar app flat>
-      <v-container>
-        <v-toolbar flat>
-          <v-toolbar-title>Photography</v-toolbar-title>
-
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn text>Home</v-btn>
-            <v-btn text>About</v-btn>
-            <v-btn text>Contact</v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-      </v-container>
-    </v-app-bar>
-  </div>
+  <v-container>
+    <v-toolbar flat class="mainHeader">
+      <v-toolbar-title>Photography</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text><router-link to="/">Home</router-link></v-btn>
+        <v-btn text><router-link to="/about">About</router-link></v-btn>
+        <v-btn text><router-link to="/contact">Contact</router-link></v-btn>
+      </v-toolbar-items>
+      <div class="hidden-sm-and-up">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-app-bar-nav-icon v-on="on"></v-app-bar-nav-icon>
+          </template>
+          <v-list class="responsiveMenu">
+            <v-list-item>
+              <v-list-item-title
+                ><router-link to="/">Home</router-link></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><router-link to="/about">About</router-link></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><router-link to="/contact"
+                  >Contact</router-link
+                ></v-list-item-title
+              >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-toolbar>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Header",
+  data: () => ({}),
+};
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,23 @@
 <template>
-  <v-app>
-    <AppBar-component />
-    <v-content> </v-content>
+  <v-app
+    ><v-app-bar flat max-height="65">
+      <AppBar-component />
+    </v-app-bar>
+    <v-content> <Hero-Component /> </v-content>
   </v-app>
 </template>
 
 <script>
 import AppBarComponent from "./components/AppBar";
+import HeroComponent from "./components/Hero";
+
 export default {
-  data(){
-    return {
-    }
+  data() {
+    return {};
   },
   components: {
     AppBarComponent,
+    HeroComponent,
   },
 };
 </script>
